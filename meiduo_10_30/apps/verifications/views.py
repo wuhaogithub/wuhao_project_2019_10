7288d68d14847da0caedacc8a22b586353e05310
xwp <xwp_fullstack@163.com>
2019-10-31 01:03:50 +0800

    短信验证码

--- meiduo_10_30/apps/verifications/views.py
+++ meiduo_10_30/apps/verifications/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from utils.response_code import RETCODE
+
 
 class ImageCodeView(View):
     def get(self, request, uuid):
@@ -17,4 +19,106 @@ class ImageCodeView(View):
         redis_conn.setex('img_%s'%uuid, 120, text)
         # ④ 返回图片
         return http.HttpResponse(image, content_type='image/jpeg')
+class SmsCodeView(View):
+    # def get(self, request, mobile):
+    #     # ① 接受数据
+    #     image_code = request.GET.get('image_code')
+    #     image_code_id = request.GET.get('image_code_id')
+    #     # ② 验证数据
+    #     if not all([image_code, image_code_id]):
+    #         return http.HttpResponseBadRequest('参数不全')
+    #     # ③ 对比用户输入的验证码与redis中的验证码书否一致
+    #     # 3.1 连接redis
+    #     from django_redis import get_redis_connection
+    #     redis_conn = get_redis_connection('code')
+    #     # 3.2 获取指定的验证码内容
+    #     redis_text = redis_conn.get('image_%s'%image_code_id)
+    #     # 3.3 判断验证码是否过期
+    #     if redis_text is None:
+    #         return http.HttpResponseBadRequest('图形验证码过期')
+    #     # 3.4 对比用户输入的验证码与redis中的是否一致
+    #     if redis_text.decode().lower() != image_code.lower():
+    #         return http.HttpResponseBadRequest('图形验证码错误')
+    #     # 3.5 判断短信是否发的太频繁
+    #     send_flag = redis_conn.get('send_flag_%s'% mobile)
+    #     if send_flag:
+    #         return http.HttpResponse({'errmsg':'短信发的太频繁了', 'code':'4001'})
+    #
+    #     # ④ 产生短信验证码
+    #     from random import randint
+    #     sms_code = '06%d' %randint(0, 999999)
+    #     # ⑤ 保存短息验证码
+    #     from apps.verifications.constants import SMS_CODE_EXPIRES_SECONDS
+    #     redis_conn.setex('sms_code_%s'%mobile, SMS_CODE_EXPIRES_SECONDS, sms_code)
+    #     # 做一个标记
+    #     redis_conn.setex('send_flag_%s'% mobile)
+    #     # ⑥ 发送短信验证码
+    #     from libs.yuntongxun.sms import CCP
+    #     CCP.send_template_sms(mobile, [send_flag,5], 1)
+    #     # ⑦ 返回响应
+    #     return http.HttpResponse({'msg':'ok', 'code':RETCODE.OK})
+
+
+
+
+
+
+    def get(self, request, mobile):
+        # ① 获取数据
+        # mobile
+        # 用户的输入的图形验证码:  image_code
+        # uuid:              image_code_id
+        image_code = request.GET.get('image_code')
+        image_code_id = request.GET.get('image_code_id')
+
+        # ② 验证数据
+        if not all([image_code, image_code_id]):
+            return http.HttpResponseBadRequest('参数不全')
+        # 省略 用户验证码长度验证
+        # 省略 image_code_id 的正则验证
+
+        # ③ 比对用户输入的验证码和redis的验证码是否一致
+        # 3.1 连接redis
+        from django_redis import get_redis_connection
+        redis_conn = get_redis_connection('code')
+        # 3.2 获取指定的数据
+        redis_text = redis_conn.get('img_%s' % image_code_id)
+        # 3.3 判断验证码是否过期
+
+        if redis_text is None:
+            return http.HttpResponseBadRequest('图片验证码以过期')
+        # 3.4 比对
+        if redis_text.decode().lower() != image_code.lower():
+            return http.HttpResponseBadRequest('图片验证码不一致')
+
+        send_flag = redis_conn.get('send_flag_%s' % mobile)
+        if send_flag:
+            return http.JsonResponse({'errmsg': '短信发送太频繁', 'code': '4001'})
+
+        # ④ 生成一个随机短信验证码
+        # 6位数值
+        from random import randint
+        sms_code = '%06d' % randint(0, 999999)
+
+        # ⑤ 保存短信验证码
+        # redis_conn.setex('sms_%s'%mobile,300,sms_code)
+        from apps.verifications.constants import SMS_CODE_EXPIRES_SECONDS
+        redis_conn.setex('sms_%s' % mobile, SMS_CODE_EXPIRES_SECONDS, sms_code)
+
+        # 添加一个标记
+        redis_conn.setex('send_flag_%s' % mobile, 60, 1)
+        # ⑥ 发送短信
+        # 免费开发测试使用的模板ID为1，形式为：
+        # 【云通讯】您使用的是云通讯短信模板，您的验证码是{1}，请于{2}分钟内正确输入。
+        # 其中{1}和{2}为短信模板的参数
+        from libs.yuntongxun.sms import CCP
+        CCP().send_template_sms(mobile,[sms_code,5],1)
+
+        # from celery_tasks.sms.tasks import send_sms_code
+        # # 任务的参数,平移到delay中
+        # # delay 中 添加函数的参数
+        # send_sms_code.delay(mobile, sms_code)
+
+        # ⑦ 返回相应
+        return http.JsonResponse({'msg': 'ok', 'code': RETCODE.OK})
 
--- meiduo_10_30/libs/yuntongxun/sms.py
+++ meiduo_10_30/libs/yuntongxun/sms.py
@@ -1,19 +1,21 @@
 # -*- coding:utf-8 -*-
+# 如果在运行当前文件,需要修改导入路径
+from libs.yuntongxun.CCPRestSDK import REST
 
-from .CCPRestSDK import REST
+# from .CCPRestSDK import REST
 
 # 说明：主账号，登陆云通讯网站后，可在"控制台-应用"中看到开发者主账号ACCOUNT SID
-_accountSid = '8aaf0708568d4143015697b0f4960888'
+_accountSid = '8aaf07086dcdca52016dd4af8069064e'
 
 # 说明：主账号Token，登陆云通讯网站后，可在控制台-应用中看到开发者主账号AUTH TOKEN
-_accountToken = '57c6c3ef3cef47e680519a734f6812f8'
+_accountToken = 'b9f59f81bd2f4d71b0f3fa1dc1c6b918'
 
 # 请使用管理控制台首页的APPID或自己创建应用的APPID
-_appId = '8aaf0708568d4143015697b0f56e088f'
+_appId = '8aaf07086dcdca52016dd4af80bd0655'
 
 # 说明：请求地址，生产环境配置成app.cloopen.com
-_serverIP = 'sandboxapp.cloopen.com'
-
+# _serverIP = 'sandboxapp.cloopen.com'
+_serverIP = 'app.cloopen.com'
 # 说明：请求端口 ，生产环境为8883
 _serverPort = "8883"
 
@@ -72,4 +74,4 @@ class CCP(object):
 if __name__ == '__main__':
     ccp = CCP()
     # 注意： 测试的短信模板编号为1
-    ccp.send_template_sms('18516952650', ['1234', 5], 1)
+    ccp.send_template_sms('15840235191', ['521521', 5], 1)
